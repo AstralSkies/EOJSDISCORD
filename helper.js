f53c968da72a355a324829ae56ab2cdc0ea13cb4
astral_skies <70440468+AstralSkies@users.noreply.github.com>
2022-10-16 19:01:26 +0100

    Modules and Functions
    Contains native and foreign functions which allow the main function act accordingly.

---
+++ helper.js
@@ -0,0 +1,108 @@
+// HelperJS will be poulated with the helper functions that are used in main.js.
+// Discord Bot: Endless Online
+// Author: AstralSkies
+// Version: 1.0
+
+// // RobotJS to simulate keyboard input.
+// npm install robotjs --save
+// https://robotjs.io/
+const robot = require('robotjs');
+
+// ffi-napi to send mouse input.
+// npm install ffi-napi --save
+// https://www.npmjs.com/package/ffi-napi
+const ffi = require('ffi-napi');
+
+// Find process module.
+// https://www.npmjs.com/package/find-process
+// npm install find-process --save
+const find = require('find-process');
+
+// Obtains the needed user32.dll functions.
+const user32 = new ffi.Library('user32', {
+    'FindWindowA': ['int32', ['string', 'string']],
+    'SetForegroundWindow': ['bool', ['int32']],
+    'SendMessageA': ['int32', ['int32', 'uint32', 'int32', 'string']]
+    });
+
+// Get the process ID
+const hwnd = user32.FindWindowA(null, "Endless Online");
+
+// Get the process ID for Discord.
+const hwnd2 = user32.FindWindowA(null, "Discord");
+
+// Get the process ID for Discord.
+find('name', 'Discord')
+    .then(function (list) {
+        // Get the process ID for Discord.
+        const discord = list[0].pid;
+        // Set the Discord window to the foreground.
+        user32.SetForegroundWindow(discord);
+        // Send the message to the Endless Online window.
+        user32.SendMessageA(hwnd, 0x100, 0, "Hello World!");
+    });
+// Read in the chatlog file.
+const fs = require('fs');
+
+
+
+const client = require('./main.js');
+
+// The path to the chatlog file.
+const chatlog = '{YOUR CHAT LOG PATH HERE}';
+
+// Read the chatlog file, return the last line and send it to Discord.
+function readChatlog(client) {
+    // Read the chatlog file.
+    const data = fs.readFileSync(chatlog, 'utf8');
+    // If there was an error reading the file.
+    if (data == null) {
+        console.log('Error reading chatlog.txt');
+        return;
+    }
+    // Split the file into an array.
+    const lines = data.split('\n');
+    // Get the last line.
+    const lastLine = lines[lines.length - 2];
+    // Return the last line to main.js.
+    return lastLine;
+}
+
+// Function which simulates keyboard input.
+function postMessage(message) {
+    user32.SetForegroundWindow(hwnd);
+    // Send the message to the Endless Online window via RobotJS
+    // With a short delay to ensure the window is in focus.
+    // DM: Smoker#7777 for more information.    
+    setTimeout(() => {
+        switch (message) {
+            case 'up':
+                robot.keyTap('up');
+                break;
+            case 'down':
+                robot.keyTap('down');
+                break;
+            case 'left':
+                robot.keyTap('left');
+                break;
+            case 'right':
+                robot.keyTap('right');
+                break;
+            case 'enter':
+                robot.keyTap('enter');
+                break;
+            default:
+                robot.typeString(message);
+                // Simulate the enter key.
+                robot.keyTap('enter');
+                break;
+        }
+    }, 100);
+    // return after the message has been sent.
+    return;
+}
+
+
+// Export the functions so they can be used in main.js.
+exports.readChatlog = readChatlog;
+exports.postMessage = postMessage;
---
+++ main.js
@@ -0,0 +1,50 @@
+// Discord Bot: Endless Online
+// Author: AstralSkies
+// Version: 1.0
+// Description: A bot that allows you to play Endless Online in Discord.
+
+// Ensure the bots previlege gateway intents are enabled.
+const Discord = { Client, 
+    GatewayIntentBits , 
+    MessageAttachment, AttachmentBuilder, EmbedBuilder} = require('discord.js');
+
+// Create a new client instance.
+const client = new Discord.Client({
+    intents: [GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent, 
+        GatewayIntentBits.GuildMessageReactions] });
+
+// Import the helper functions readChatlog() and sendChatlog().
+const helper ={ readChatlog } = require('./helper.js');
+
+// Embed the message returned from the helper function.
+  const embed = new EmbedBuilder()
+  .setColor('#0099ff')
+  .setTitle('Chatlog')
+  .setDescription(helper.readChatlog())
+
+// Check if the bot is ready
+client.once('ready', () => {
+    console.log('I am ready!');
+    // Call the ReadChatlog() function every 10seconds.
+    setInterval(() => {
+        // Read the chatlog file.
+        client.channels.cache.get('{YOUR CHANNEL HERE}').send({ embeds: [embed] });
+    }
+    , 10000);
+});
+
+// If the user has stated a message in the channel.
+client.on('messageCreate', message => {
+    // Call the postMessage function. If the message is not from the bot.
+    if (message.author.bot == false) {
+        helper.postMessage(message.content);
+    }
+});
+
+// Obtain the token from the json folder and log in.
+client.login(require('./json/token.json').token);
+
+// Export the client so it can be used in other files.
+module.exports = client;
